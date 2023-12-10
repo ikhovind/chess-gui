6130d058d10cef740c068d255c207b4905335b9e
Ingebrigt Hovind <ing.hovind@hotmail.com>
2023-12-10 01:27:34 +0100

    try to run both in one dockerfile

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var whiteSquareGrey = '#a9a9a9'
 var blackSquareGrey = '#696969'
 
 window.addEventListener('load', function () {
-  var socket = new WebSocket('wss://sjakkmotor.ikhovind.no:3389');
+  var socket = new WebSocket('ws://localhost:3389');
 
 // Connection opened
   socket.addEventListener('open', function (event) {
